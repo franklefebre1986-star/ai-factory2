a9c657dd2304371365f014b02d6e55b85cc39fa2
franklefebre1986-star <franklefebre1986@gmail.com>
2025-12-11 14:04:27 +0100

    Update route.js

--- app/api/generate-image/route.js
+++ app/api/generate-image/route.js
@@ -2,15 +2,21 @@ export async function POST(req) {
   const { prompt } = await req.json();
 
   const response = await fetch(
-    "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2",
+    "https://api-inference.huggingface.co/models/stabilityai/sd-turbo",
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ inputs: prompt }),
+      body: JSON.stringify({
+        inputs: prompt,
+      }),
     }
   );
+
+  if (!response.ok) {
+    return new Response("Error generating image", { status: 500 });
+  }
 
   const arrayBuffer = await response.arrayBuffer();
   const base64 = Buffer.from(arrayBuffer).toString("base64");
